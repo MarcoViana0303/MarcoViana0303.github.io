0a2c357fc9353f93969cb827fe363f350f899349
Marco Viana <marcoviana100@gmail.com>
2023-04-12 21:43:28 -0300

    feat: Função darkmode ainda em manutenção

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,11 @@ const [darkMode, setDarkMode] = useState(false);
  const messageTemporary = () => {
   alert('Botão dark-mode temporariamente desligado.');
   setDarkMode(!darkMode);
+  // document.body.style.backgroundColor = darkMode ? '#fff' : 'black'
+  // document.body.style.color = darkMode ? '#0f0c27' : 'white'
  };
+
+
     return(
          <header>
        <a className="navbar-brand opacity-50 logo" href="#home">{'<Marco Viana />'}</a>
@@ -19,7 +23,7 @@ const [darkMode, setDarkMode] = useState(false);
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projetos</a></li>
         <li><a href="#contact">Contato</a></li>
-        <div className="bx bxs-ghost" id="darkmode" onClick={messageTemporary}></div>
+        <div className={darkMode ? 'bx bx-sun' : 'bx bxs-moon' } id="darkmode" onClick={messageTemporary}></div>
         { darkMode && <audio autoPlay >
   <source src={ require('../Audio/files_theme2.mp3')} type="audio/mp3" />
 </audio> }
